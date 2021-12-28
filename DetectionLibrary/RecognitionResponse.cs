97ae6ce4275ccc300ffbd2ab54285360c8cbacf0
alexkorol13 <alexkorol13@users.noreply.github.com>
2021-12-28 03:44:03 +0300

    Lab 1 commit

---
+++ DetectionLibrary/RecognitionResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library
+{
+    public class RecognitionResponse
+    {
+        public Dictionary<string, int> Objects { get; set; }
+        public int progress { get; set; }
+        public List<List<float>> Rectangles { get; set; }
+        public RecognitionResponse()
+        {
+            Objects = new Dictionary<string, int>();
+            Rectangles = new List<List<float>>();
+            progress = 0;
+        }
+    }
+}
